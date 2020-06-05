946b17074b6df1dffc5397219b269a2f904e9d2a
Syrus Dark <naveen@syrusdark.website>
2020-06-05 18:51:09 +0530

    Writing Documentation

--- imgtopdfeasy/__init__.py
+++ imgtopdfeasy/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+'''This file starts the CLI
+'''
 from . import img2pdf
 
 __version__ = '0.2.0'
 def main():
+    '''Calling CLI
+    '''
     img2pdf.parse_cli()
 
--- imgtopdfeasy/img2pdf.py
+++ imgtopdfeasy/img2pdf.py
@@ -1,9 +1,50 @@
 #!/usr/bin/env python
+'''This module defines the funtions necessary for PDF conversion as well as CLI from argparse.
+
+Has the following function:
+    img2pdf()
+    convertToPageSize()
+    parse_cli()
+'''
 def img2pdf(input,output,extension):
-    from PIL import Image
+    '''This function convert images to PDF of sepcific extension and saves in specific location.
+    Parameters
+    ----------
+    input : path
+        Input file folder to search images , in `input` directory.
+    output : path
+        Place to save output file (Doesn't need .pdf extension)
+    extension : str
+        The extension for Images to search for. Supports the images supported by Pillow. Eg. `jpeg`,`png etc.
+
+    Returns
+    -------
+    str
+        Filepath stored of PDF stored. Sucessfully saved ay `filepath`
+    Raises
+    ------
+    FileNotFoundError
+        If there is no files of given extension in the `input`.
+    Examples
+    --------
+    >>> imgtopdfeasy.img2pdf.img2pdf(r"D:\Chemistry\Chemistry",r"D:\chemistrydemo","jpeg")
+    D:\Chemistry\Chemistry\1.jpeg
+    D:\Chemistry\Chemistry\2.jpeg
+    D:\Chemistry\Chemistry\3.jpeg
+    D:\Chemistry\Chemistry\4.jpeg
+    D:\Chemistry\Chemistry\5.jpeg
+    D:\Chemistry\Chemistry\6.jpeg
+    D:\Chemistry\Chemistry\7.jpeg
+    'Sucessfully saved at D:\\chemistrydemo.pdf'
+
+    '''
     from pathlib import Path
+    from PIL import Image
+    
     imagelist=[]
     for path in Path(input).rglob('*.'+extension):
+        if path==None:
+            raise FileNotFoundError("NO files with Specified extension in the folder")
         im=Image.open(path)
         im1=im.convert('RGB')
         imagelist.append(im1)
@@ -14,19 +55,30 @@ def img2pdf(input,output,extension):
     im1=im.convert('RGB')
     im1.save(output+'.pdf',save_all=True,append_images=imagelist[1:])
     return 'Sucessfully saved at '+output+'.pdf'
-def convertToPageSize(imgPath,height,width):
+def convertToPageSize(imgPath,height,width,border):
     '''This converts the image so same size as specified. It doesn't scale it but creates a white background on it.
     A white background on which the image is pasted.
+
+    Parameters
+    ----------
+    imgPath : path
+        Location of Image which need background.
+    height : int
+        Height
+
     '''
     from PIL import Image
     whiteBackground=Image.new('RGB',(height,width),color="#fff")
     mainImage=Image.open(imgPath)
     mainImageWidth,mainImageHeight=mainImage.size[0],mainImage.size[1]
-    if height>mainImageHeight:
+    if height-border>mainImageHeight:
+        #Image height is inside background height
         pass
     whiteBackground.save(r"E:\hello.jpg")
     whiteBackground.show()
 def parse_cli():
+    '''This is the CLI for img2pdf using argparse.
+    '''
     import argparse
     parser=argparse.ArgumentParser()
     parser.add_argument('-i','--input',help='Input file folder full path. \n Realtive or abosolute',required=True)
@@ -37,4 +89,3 @@ def parse_cli():
     outputPath=args.output
     extension=args.extension
     print(img2pdf(inputPath,outputPath,extension))
-convertToPageSize(1000,1000)
