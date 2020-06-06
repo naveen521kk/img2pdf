7482f49ec9b7d8aee1125bb226d6bfce5ed71f38
Syrus Dark <naveen@syrusdark.website>
2020-06-06 19:52:32 +0530

    Started working on background

--- imgtopdfeasy/img2pdf.py
+++ imgtopdfeasy/img2pdf.py
@@ -6,7 +6,7 @@ Has the following function:
     convertToPageSize()
     parse_cli()
 '''
-def img2pdf(input,output,extension):
+def img2pdf(input, output, extension):
     '''This function convert images to PDF of sepcific extension and saves in specific location.
     Parameters
     ----------
@@ -15,7 +15,8 @@ def img2pdf(input,output,extension):
     output : path
         Place to save output file (Doesn't need .pdf extension)
     extension : str
-        The extension for Images to search for. Supports the images supported by Pillow. Eg. `jpeg`,`png etc.
+        The extension for Images to search for. Supports the images supported by Pillow.
+        Eg. `jpeg`,`png etc.
 
     Returns
     -------
@@ -39,23 +40,22 @@ def img2pdf(input,output,extension):
 
     '''
     from pathlib import Path
-    from PIL import Image
-    
-    imagelist=[]
+    from PIL import Image 
+    imagelist = []
     for path in Path(input).rglob('*.'+extension):
-        if path==None:
+        if path == None:
             raise FileNotFoundError("NO files with Specified extension in the folder")
-        im=Image.open(path)
-        im1=im.convert('RGB')
+        im = Image.open(path)
+        im1 = im.convert('RGB')
         imagelist.append(im1)
         print(path)
     for path in Path(input).rglob('*.'+extension):
-        im=Image.open(path)
+        im = Image.open(path)
         break
-    im1=im.convert('RGB')
+    im1 = im.convert('RGB')
     im1.save(output+'.pdf',save_all=True,append_images=imagelist[1:])
     return 'Sucessfully saved at '+output+'.pdf'
-def convertToPageSize(imgPath,height,width,border):
+def convertToPageSize(imgPath, height, width, border, bgColor):
     '''This converts the image so same size as specified. It doesn't scale it but creates a white background on it.
     A white background on which the image is pasted.
 
@@ -64,17 +64,23 @@ def convertToPageSize(imgPath,height,width,border):
     imgPath : path
         Location of Image which need background.
     height : int
-        Height
-
+        Height in px.  As PIL standard.
+    width : int
+        Width in px. As PIL standard
+    border : int
+        Border width of the image.
+    bgColor : ColorHex
+        Colour of background.
     '''
     from PIL import Image
-    whiteBackground=Image.new('RGB',(height,width),color="#fff")
+    whiteBackground=Image.new('RGB',(width,height),color=bgColor)
     mainImage=Image.open(imgPath)
     mainImageWidth,mainImageHeight=mainImage.size[0],mainImage.size[1]
-    if height-border>mainImageHeight:
+    if height-2*(border)>mainImageHeight and width-2*(border)>mainImageWidth:
         #Image height is inside background height
+        whiteBackground.paste(mainImage,box=(border,(height-2*(border)-mainImageHeight)//2))
+    else:
         pass
-    whiteBackground.save(r"E:\hello.jpg")
     whiteBackground.show()
 def parse_cli():
     '''This is the CLI for img2pdf using argparse.
@@ -89,3 +95,4 @@ def parse_cli():
     outputPath=args.output
     extension=args.extension
     print(img2pdf(inputPath,outputPath,extension))
+print(convertToPageSize(r"C:\Users\Naveen User\Pictures\Capture.png",700,1300,100,"#000"))
